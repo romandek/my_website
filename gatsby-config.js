5a142f4f466dd31367e7e123de987b71754d9abf
Roman Dek <8047387+romandek@users.noreply.github.com>
2020-08-16 15:53:53 +0300

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,87 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+      siteTitle: `Roman Dek`,
+      siteTitleAlt: `Roman Dek — data analyst, developer, teacher`,
+      siteHeadline: `Roman Dek`,
+      siteUrl: `https://romnadek.com`,
+      siteDescription: `Personal website of Roman Dek.`,
+      siteLanguage: `en`,
+      siteImage: `/banner.jpg`,
+      author: `@romandek`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `http://rdek.me/twitter`,
+          },
+          {
+            name: `GitHub`,
+            url: `http://rdek.me/git`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Roman Dek`,
+        short_name: `Roman Dek`,
+        description: `Personal website of Roman Dek.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
